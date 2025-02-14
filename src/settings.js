8045d371f5c1600ea944b2b4126e92fc15c27a1c
JatinnVaityy <vaityjatin13@gmail.com>
2025-02-14 22:07:01 +0530

    initial commit

--- src/settings.js
+++ src/settings.js
@@ -1,13 +1,6 @@
-/**
- * Created by Tomasz Gabrysiak @ Infermedica on 06/03/2017.
- *
- * Please provide your own app-id and app-key below.
- * To obtain keys please register at https://developer.infermedica.com.
- */
-
 const settings = {
-  'app-id': '',
-  'app-key': ''
+  'app-id': 'e29afda8',
+  'app-key': 'aa7b6e298959bc41b95ba4746a7bbde0'
 };
 
 export default settings;
